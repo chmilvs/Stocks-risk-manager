dfffab16a26a22730cda6f13d7fd99932fa52768
Sunwader <sunwader@gmail.com>
2021-02-05 15:45:21 +0300

    Commit 4: RiskPage

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar'
 import Banner from './components/Banner/Banner'
-import AuthForm from './components/AuthForm/AuthForm' 
+import AuthForm from './components/AuthForm/AuthForm'
+import RiskPage from "./components/RiskPage/RiskPage";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
   return (
@@ -9,14 +11,22 @@ function App() {
       <NavBar/>
       <Switch>
 
-      <Route exact path='/'>
-      <Banner/>
-      </Route>
+          <Route exact path='/'>
+            <Banner/>
+          </Route>
 
-      <Route path='/auth'>
-      <AuthForm/>
-      </Route>
- 
+          <Route path='/auth'>
+            <AuthForm/>
+          </Route>
+
+          <Route path='/riskpage'>
+              <RiskPage/>
+          </Route>
+
+          <Route path='/dashboard'>
+              <Dashboard/>
+          </Route>
+
       </Switch>
 
     </BrowserRouter>
---
+++ front/src/components/RiskPage/RiskPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import RiskForm from "./RiskForm/RiskForm";
+import Stocks from "./Stocks/Stocks";
+import Chart from "./Chart/Chart";
+
+
+function RiskPage() {
+    return (
+        <>
+            <RiskForm/>
+            <Stocks/>
+            <Chart/>
+        </>
+    );
+}
+
+export default RiskPage;
