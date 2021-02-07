345b085b046c23b9388994e1df47f1527c94620f
Sergey Emelianov <oldskuul@mail.ru>
2021-02-07 11:19:33 +0300

    chart added to Riskpage (with ticker finder) v1

--- front/src/components/Dashboard/Dashboard.js
+++ front/src/components/Dashboard/Dashboard.js
@@ -2,16 +2,12 @@ import PieDiagram from './PieDiagram/PieDiagram';
 import BarDiagram from './BarDiagram/BarDiagram';
 import Table from './Table/Table'
 import './Dashboard.css'
-import StockChartInfo from '../StockChartInfo/StockChartInfo'
 
 function Dashboard() {
   return (
     <div className="dashboard">
     <div className="table">
       <Table/>
-    <div>
-    <StockChartInfo/>
-    </div>
     </div>
     <div className="diagrams">
       <PieDiagram/>
