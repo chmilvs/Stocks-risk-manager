cdeb4a9297163e0217872b2fcc43175aad368049
Sergey Emelianov <oldskuul@mail.ru>
2021-02-08 10:15:04 +0300

    chart added to Riskpage (with ticker finder) v2

--- front/src/components/StockChartInfo/StockChartInfo.js
+++ front/src/components/StockChartInfo/StockChartInfo.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area } from 'recharts';
+import { AreaChart, XAxis, YAxis, CartesianGrid, Brush, Legend, ReferenceLine, Tooltip, Area } from 'recharts';
 
 function StockChartInfo(props) {
   const [info, setInfo] = useState(null)
@@ -21,48 +21,45 @@ function StockChartInfo(props) {
   let data = [];
   let data2 = []
 
-  // if (info) {
-  //   for (let key in info["Time Series (Daily)"]) {
-  //     data2.push(key)
-  //   }
-  // }
+  if (info) {
+    for (let key in info["Time Series (Daily)"]) {
+      data2.push(key)
+    }
+  }
 
   info && Object.values(info["Time Series (Daily)"]).map((el, index) => {
     return data.push({ "цена": Number(el["4. close"]), "дата": data2[index] })
   })
-
-
-
+  
 
   console.log(data2)
-  console.log(data);
+  console.log(data);  
 
   return (
     <>
-    <div style={{ marginTop:"210px"}}>
-      <div style={{ marginLeft: "80px"}}>
-        <form onSubmit={getInfo}>
-          <input type="text" onChange={inputHandler}></input>
-          <button className="button primary" type="submit">Поиск по тикеру</button>
-        </form>
+      <div style={{ marginTop: "210px" }}>
+        <div style={{ marginLeft: "160px" }}>
+          <form onSubmit={getInfo}>
+            <input type="text" onChange={inputHandler}></input>
+            <button className="button primary" type="submit">Поиск по тикеру</button>
+          </form>
 
-      </div>
-      {data && <AreaChart width={500} height={380} data={data.reverse().slice(0, 50)}
-        margin={{ top: 20, right: 20, left: 20, bottom: 20 }}>
-        <defs>
-          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="2%" stopColor="#8884d8" stopOpacity={0.8} />
-            <stop offset="90%" stopColor="#8884d8" stopOpacity={0} />
-          </linearGradient>
-        </defs>
-
-        <YAxis />
-        {/* <CartesianGrid strokeDasharray="3 3" /> */}
-        <Tooltip viewBox={{ x: 0, y: 0, width: 1000, height: 500 }}/>
-        <Area type="monotone" dataKey="дата" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-        <Area type="monotone" dataKey="цена" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-
-      </AreaChart>}
+        </div>
+        {data && <AreaChart width={710} height={380} data={data.reverse().slice(50, 100)}
+          margin={{ top: 20, right: 150, left: 100, bottom: 20 }}>
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="2%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="70%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          {/* <XAxis dataKey="дата"/> */}
+          <YAxis /> 
+          <Brush dataKey="дата" height={30} stroke="#8884d8"/>
+          <Area type="monotone" dataKey="цена" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+          {/* <Tooltip formatter={(label) => label + " %"}/> */}
+          <Tooltip />
+        </AreaChart>}
       </div>
     </>
   );
