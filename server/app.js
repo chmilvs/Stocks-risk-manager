3cbb0ced52c568b2e94f1f17fc32da3a48aeb6ef
Sendoff74 <sendoff74@gmail.com>
2021-02-09 15:46:33 +0300

    Bac statuses+stock

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const useMiddleware = require('./middlewares/index');
 const useErrorHandlers = require('./middlewares/error-handlers');
+const stockRouter = require('./routes/stock')
 const authRouter = require('./routes/auth')
 
 useMiddleware(app);
@@ -9,5 +10,6 @@ useMiddleware(app);
 // useErrorHandlers(app);
 
 app.use('/api/auth', authRouter)
+app.use('api/stock',stockRouter)
 
 module.exports = app;
