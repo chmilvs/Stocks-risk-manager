893b97412ae5a3ffc17134e4910289320dcb111e
Victoria <victoria.chmil@yandex.ru>
2021-02-09 14:45:14 +0300

    adaptiv(net)

--- front/src/components/NavBar/NavBar.js
+++ front/src/components/NavBar/NavBar.js
@@ -2,10 +2,10 @@ import {Link, useHistory} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
 import './NavBar.css'
 import {logOutAC} from '../../redux/actionCreators/authAC';
+import {useRef} from 'react'
 
 function NavBar() {
     const user = useSelector(state => state.auth)
-    console.log(user);
     const dispatch = useDispatch()
     const history = useHistory()
     const logOut = (event) => {
@@ -13,41 +13,72 @@ function NavBar() {
         dispatch(logOutAC())
         history.push('/')
     }
+
+    const wrapperRef = useRef()
+    const handleMedia = (event) => {
+      event.preventDefault()
+      const wrappperMedia = wrapperRef.current
+      wrappperMedia.classList.toggle('is-nav-open')
+    }
     return (
-        <header id="header">
-            <h1 id="logo">
-                <Link to="/">Investhood Helper</Link>
-            </h1>
-            <nav id="nav">
-                <ul>
-                    {user.isLogged ? (
-                        <>
-                            <li>
-                                <Link to="/riskpage" className="button primary">
-                                    Расчет риска
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/dashboard" className="button primary">
-                                    Профиль
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to={'/'} onClick={logOut} className="button primary">
-                                    Выйти
-                                </Link>
-                            </li>
-                        </>
-                    ) : (
-                        <li>
-                            <Link to="/auth" className="button primary">
-                                Войти
-                            </Link>
-                        </li>
-                    )}
-                </ul>
-            </nav>
-        </header>
+      <header id="header">
+        <button onClick={handleMedia} className="forMedia">
+          <i className="fa fa-outdent"></i>
+        </button>
+        <div ref={wrapperRef} className="wrapper-media">
+          <div className="nav-media">
+            <div className="nav__body-media">
+              <li>
+                <Link to="/riskpage" className="">
+                  Расчет риска
+                </Link>
+              </li>
+              <li>
+                <Link to="/dashboard" className="">
+                  Профиль
+                </Link>
+              </li>
+              <li>
+                <Link to={"/"} onClick={logOut} className="">
+                  Выйти
+                </Link>
+              </li>
+            </div>
+          </div>
+        </div>
+        <h1 id="logo">
+          <Link to="/">Investhood Helper</Link>
+        </h1>
+        <nav id="nav">
+          <ul>
+            {user.isLogged ? (
+              <>
+                <li>
+                  <Link to="/riskpage" className="">
+                    Расчет риска
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/dashboard" className="">
+                    Профиль
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"/"} onClick={logOut} className="">
+                    Выйти
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <li>
+                <Link to="/auth" className="">
+                  Войти
+                </Link>
+              </li>
+            )}
+          </ul>
+        </nav>
+      </header>
     );
 }
 
