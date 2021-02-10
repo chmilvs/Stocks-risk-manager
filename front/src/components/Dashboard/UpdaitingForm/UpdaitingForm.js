fd65a85aed9059c52cf70363991472793aa25b9d
Victoria <victoria.chmil@yandex.ru>
2021-02-10 16:26:31 +0300

    Updating

--- front/src/components/Dashboard/UpdaitingForm/UpdaitingForm.js
+++ front/src/components/Dashboard/UpdaitingForm/UpdaitingForm.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-function UpdaitingForm(props) {
-
+function UpdaitingForm() {
+  const data = useSelector(state => state.auth.currentUser)
+  console.log(data);
   return (
-    <div>
-     <label>Введите новые данные</label> 
-    </div>
+    <>
+      <form>
+      <label>Введите новые данные:</label>
+        <div className="col-6 col-12-xsmall">
+          <input type="text" name="username" placeholder={`${data.username}`} />
+        </div>
+        <div className="col-6 col-12-xsmall">
+          <input type="password" name="password" placeholder="Новый пароль" />
+        </div>
+        <div className="col-6 col-12-xsmall">
+          <input type="text" name="phone" placeholder={`${data.phone}`} />
+        </div>
+        <div className="col-6 col-12-xsmall">
+          <input type="email" name="email" placeholder={`${data.email}`} />
+        </div>
+        <div className="col-6 col-12-xsmall">
+          <input type="email" name="email" placeholder={`${data.deposit}`} />
+        </div>
+        <button className="button primary small">Обновить</button>
+      </form>
+    </>
   );
 }
 
