2ce718af2776a3ce9922ba9256ddd0a56ad038de
Sergey E <oldskuul@mail.ru>
2021-02-11 20:27:03 +0300

    changes 1

--- front/src/components/CalculationField/CalculationField.js
+++ front/src/components/CalculationField/CalculationField.js
@@ -1,31 +1,24 @@
 import React, {useEffect, useState} from 'react';
-import { useDispatch } from 'react-redux';
-import { addStockAC } from '../../redux/actionCreators/stockAC';
+
+
 import {useHistory} from 'react-router-dom';
-// import { handleSubmit } from '../../fetchFunctions/fetchFunction'
+
 import ModalWindow from '../ModalWindow/ModalWindow'
+import InputStocks from "../StockChartInfo/InputStocks";
 
 function CalculationField({loading, sumToSpend, info, actualPrice, tickerName}) {
-  const dispatch = useDispatch()
+
   const [valueToBuy, setValueToBuy] = useState(Math.floor(sumToSpend / actualPrice))
   const history = useHistory()
   const [open, setOpen] = useState(false)
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setOpen(true);
-    const {
-      inquiry: { value: inquiry },
-    } = event.target;
-    dispatch(addStockAC({ inquiry, actualPrice, tickerName }));
-  };
-  useEffect(() => {
-    setValueToBuy(Math.floor(sumToSpend / actualPrice));
-  }, [sumToSpend, actualPrice]);
-
+  const [text, setText] = useState('Акции добавлены в портфель!')
   const handleClick = () => {
     setOpen(false)
     history.push('/dashboard')
   }
+  useEffect(() => {
+    setValueToBuy(Math.floor(sumToSpend / actualPrice));
+  }, [sumToSpend, actualPrice, setValueToBuy]);
 
     return (
         <div>
@@ -34,21 +27,9 @@ function CalculationField({loading, sumToSpend, info, actualPrice, tickerName}) 
                     Вывод:
                     <li>Текущая стоимость акции: {actualPrice} USD</li>
                     <li>Максимальное количеcтво акций к покупке: {valueToBuy} </li>
-                    <form style={{marginTop: "10px"}} onSubmit={handleSubmit}>
-                        <input name="inquiry" type="number" onClick={e=>e.target.value=''} value={valueToBuy} placeholder="Или введите свои данные"></input>
-                        <button
-                            style={{
-                                marginTop: "10px",
-                                marginBottom: "100px",
-                                height: "3em",
-                                fontSize: "15pt",
-                            }}
-                            className="button primary">
-                            Добавить
-                        </button>
-                    </form>
+                    <InputStocks setOpen={setOpen} actualPrice={actualPrice} tickerName={tickerName} valueToBuy={valueToBuy} setText={setText}/>
                 </ul> : null}
-                    <ModalWindow open={open} setOpen={setOpen} handleClick={handleClick} text={'Акции добавлены в портфель!'} />
+                    <ModalWindow open={open} setOpen={setOpen} handleClick={handleClick} text={text} />
             </div>}
         </div>
     );
---
+++ front/src/components/StockChartInfo/InputStocks.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import {addStockAC} from "../../redux/actionCreators/stockAC";
+import {useDispatch, useSelector} from "react-redux";
+
+function InputStocks({setOpen, actualPrice, tickerName, valueToBuy, setText}) {
+    const dispatch = useDispatch()
+    const deposit = useSelector(state => state.auth.currentUser.deposit)
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setOpen(true);
+        const {
+            inquiry: {value: inquiry},
+        } = event.target;
+        if (inquiry * valueToBuy <= deposit) dispatch(addStockAC({inquiry, actualPrice, tickerName}))
+        else setText(`Превышена сумма имеющихся средств`)
+    }
+    return (
+        <div>
+            <form key={valueToBuy} style={{marginTop: "10px"}} onSubmit={handleSubmit}>
+                <input name="inquiry" type="number" defaultValue={valueToBuy} ></input>
+                <button
+                    style={{
+                        marginTop: "10px",
+                        marginBottom: "100px",
+                        height: "3em",
+                        fontSize: "15pt",
+                    }}
+                    className="button primary">
+                    Добавить
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default InputStocks;
