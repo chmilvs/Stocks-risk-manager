53ad9ff745d6b98bd6e9ac9e8525d494bfe29ed2
Sergey E <oldskuul@mail.ru>
2021-02-11 13:24:05 +0300

    pie chart update v1

--- front/src/components/Dashboard/Dashboard.js
+++ front/src/components/Dashboard/Dashboard.js
@@ -1,19 +1,55 @@
+import {useState, useEffect} from 'react'
 import PieDiagram from './PieDiagram/PieDiagram';
 import BarDiagram from './BarDiagram/BarDiagram';
 import Table from './Table/Table'
 import './Dashboard.css'
-import { useSelector } from 'react-redux';
+import {useSelector} from 'react-redux';
+import {API_KEY, GET_COMPANY_NAMES} from "../../redux/utils/utils";
 
 function Dashboard() {
-  const deposit = useSelector(state => state.auth.currentUser.deposit)
+    const deposit = useSelector(state => state.auth.currentUser.deposit)
+    const stocks = useSelector(state => state.auth.currentUser.stocks)
+    const [loading, setLoading] = useState([])
+    const expired = 'Apikey expired'
+    useEffect(() => {
+        const textArr = stocks
+            .map((el) => {
+                return el.tickerName;
+            })
+            .join();
+        if (stocks.length > 0) {
+            fetch(`${GET_COMPANY_NAMES}${textArr}${API_KEY}`)
+                .then((res) => res.json())
+                .then((stockss) => {
+                    console.log(stockss.status);
+                    if (!stockss["Error Message"]) {
+                        stockss.map((el) => {
+                            stocks.forEach(defaultStock => {
+                                if (defaultStock.tickerName == el.symbol) {
+                                    defaultStock.companyName = el.name
+                                    defaultStock.actualPrice = el.price
+                                }
+                            })
+                        })
+                    } else {
+                        stocks.forEach(el => {
+                            el.companyName = expired
+                            el.actualPrice = expired
+                        })
+                    }
+                    setLoading(stocks);
+                });
+        }
+    }, [stocks]);
+    console.log(loading)
     return (
         <div className="dashboard">
             <div className="table">
-            Мой <i class="fa fa-briefcase">: {deposit} USD</i>
-                <Table/>
+                Мой <i class="fa fa-briefcase">: {Number(deposit).toFixed(2)} USD</i>
+                <Table loading={loading} expired={expired}/>
             </div>
             <div className="diagrams">
-                <PieDiagram/>
+                <PieDiagram loading={loading}/>
                 <BarDiagram/>
             </div>
         </div>
--- front/src/components/Dashboard/PieDiagram/PieDiagram.js
+++ front/src/components/Dashboard/PieDiagram/PieDiagram.js
@@ -1,57 +1,38 @@
-import {Cell, Pie, PieChart} from "recharts";
+import {Cell, Pie, PieChart, Tooltip} from "recharts";
 
-function PieDiagram() {
-    const data = [
-        {name: "Group A", value: 400},
-        {name: "Group B", value: 300},
-        {name: "Group C", value: 300},
-        {name: "Group D", value: 200},
-    ];
+function PieDiagram({loading}) {
     const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
     const RADIAN = Math.PI / 180;
-    const renderCustomizedLabel = ({
-                                       cx,
-                                       cy,
-                                       midAngle,
-                                       innerRadius,
-                                       outerRadius,
-                                       percent,
-                                       index,
-                                   }) => {
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
         const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
         const x = cx + radius * Math.cos(-midAngle * RADIAN);
         const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
         return (
-            <text
-                x={x}
-                y={y}
-                fill="white"
-                textAnchor={x > cx ? "start" : "end"}
-                dominantBaseline="central"
-            >
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
                 {`${(percent * 100).toFixed(0)}%`}
             </text>
         );
     };
-
     return (
         <>
-            <PieChart width={400} height={350}>
+            <PieChart width={500} height={350}>
                 <Pie
-                    data={data}
+                    data={loading}
                     cx={200}
                     cy={150}
                     labelLine={false}
                     label={renderCustomizedLabel}
-                    outerRadius={100}
+                    outerRadius={130}
                     fill="#8884d8"
-                    dataKey="value"
+                    dataKey="amountBuyed"
+                    nameKey='companyName'
                 >
-                    {data.map((entry, index) => (
+                    {loading.map((entry, index) => (
                         <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
                     ))}
                 </Pie>
+                <Tooltip />
             </PieChart>
         </>
     );
