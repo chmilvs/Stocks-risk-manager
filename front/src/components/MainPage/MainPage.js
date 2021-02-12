ba3e38da3b47b41648798f0f9cda6dabbcef3fc7
Sunwader <sunwader@gmail.com>
2021-02-12 14:19:54 +0300

    mainpage fix

--- front/src/components/MainPage/MainPage.js
+++ front/src/components/MainPage/MainPage.js
@@ -7,7 +7,7 @@ function MainPage() {
             <section id="banner">
                 <div className="content">
                     <header>
-                        <h2 id="banner-title">Toffee</h2>
+                        <h2 id="banner-title">Toffee</h2><br/>
                         <p id="banner-text">Риск-менеджер для акций компаний американского рынка.
                             Больше никаких необдуманных покупок.</p>
                     </header>
@@ -17,48 +17,48 @@ function MainPage() {
             </section>
 
 
-            <section id="one" className="spotlight style1 bottom">
-                <span className="image fit main"><img src="images/pic02.jpg" alt=""/></span>
-                <div className="content">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-4 col-12-medium">
-                                <header>
-                                    <h2>Odio faucibus ipsum integer consequat</h2>
-                                    <p>Nascetur eu nibh vestibulum amet gravida nascetur praesent</p>
-                                </header>
-                            </div>
-                            <div className="col-4 col-12-medium">
-                                <p>Toffee поможет вам диверсифицировать средства,
-                                    вложенные в акции компаний и выбрать их оптимальную долю в портфеле,
-                                    исходя из размера капитала и заданного процента риска.</p>
-                            </div>
-                            <div className="col-4 col-12-medium">
-                                <p>Morbi enim nascetur et placerat lorem sed iaculis neque ante
-                                    adipiscing adipiscing metus massa. Blandit orci porttitor semper.
-                                    Arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer
-                                    mi sed nascetur cep aliquet augue varius tempus. Feugiat lorem
-                                    ipsum dolor nullam.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            {/*<section id="one" className="spotlight style1 bottom">*/}
+            {/*    <span className="image fit main"><img src="images/pic02.jpg" alt=""/></span>*/}
+            {/*    <div className="content">*/}
+            {/*        <div className="container">*/}
+            {/*            <div className="row">*/}
+            {/*                <div className="col-4 col-12-medium">*/}
+            {/*                    <header>*/}
+            {/*                        <h2>Odio faucibus ipsum integer consequat</h2>*/}
+            {/*                        <p>Nascetur eu nibh vestibulum amet gravida nascetur praesent</p>*/}
+            {/*                    </header>*/}
+            {/*                </div>*/}
+            {/*                <div className="col-4 col-12-medium">*/}
+            {/*                    <p>Toffee поможет вам диверсифицировать средства,*/}
+            {/*                        вложенные в акции компаний и выбрать их оптимальную долю в портфеле,*/}
+            {/*                        исходя из размера капитала и заданного процента риска.</p>*/}
+            {/*                </div>*/}
+            {/*                <div className="col-4 col-12-medium">*/}
+            {/*                    <p>Morbi enim nascetur et placerat lorem sed iaculis neque ante*/}
+            {/*                        adipiscing adipiscing metus massa. Blandit orci porttitor semper.*/}
+            {/*                        Arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer*/}
+            {/*                        mi sed nascetur cep aliquet augue varius tempus. Feugiat lorem*/}
+            {/*                        ipsum dolor nullam.</p>*/}
+            {/*                </div>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
 
-            <section id="two" className="spotlight style1 bottom">
-                <span className="image fit main"><img src="images/pic02.jpg" alt=""/></span>
-                <div className="content">
-                    <div className="container">
-                        <p>
-                            Morbi enim nascetur et placerat lorem sed iaculis neque ante
-                                    adipiscing adipiscing metus massa. Blandit orci porttitor semper.
-                                    Arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer
-                                    mi sed nascetur cep aliquet augue varius tempus. Feugiat lorem
-                                    ipsum dolor nullam.
-                        </p>
-                    </div>
-                </div>
-            </section>
+            {/*<section id="two" className="spotlight style1 bottom">*/}
+            {/*    <span className="image fit main"><img src="images/pic02.jpg" alt=""/></span>*/}
+            {/*    <div className="content">*/}
+            {/*        <div className="container">*/}
+            {/*            <p>*/}
+            {/*                Morbi enim nascetur et placerat lorem sed iaculis neque ante*/}
+            {/*                        adipiscing adipiscing metus massa. Blandit orci porttitor semper.*/}
+            {/*                        Arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer*/}
+            {/*                        mi sed nascetur cep aliquet augue varius tempus. Feugiat lorem*/}
+            {/*                        ipsum dolor nullam.*/}
+            {/*            </p>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
         </>
     );
 }
