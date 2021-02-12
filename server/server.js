0daa3bb42b72bf2ab57127bbad398c85a2ed3be2
Victoria <victoria.chmil@yandex.ru>
2021-02-12 12:36:26 +0300

    X

--- server/server.js
+++ server/server.js
@@ -3,26 +3,9 @@ const http = require('http');
 const app = require('./app');
 
 const port = process.env.PORT || 4000;
-// const wsPort = process.env.PORT || 8080
 
 const server = http.createServer(app);
-// const wsServer = http.createServer()
 
-// const io = require('socket.io')(wsServer, {
-//     serveClient: false,
-//     pingInterval: 10000,
-//     pingTimeout: 5000,
-//     cookie: false
-// });
-
-
-
-
-
-// wsServer.listen(
-//   wsPort,()=>{
-//     console.log('WS server started on '+ wsPort)
-// })
 server.listen(port, () => console.log(`
 ***
 Listen to server${port}
